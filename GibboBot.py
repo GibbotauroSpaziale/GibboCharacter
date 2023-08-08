5c0259002ab618992e8f46b6922df04151140fa2
GibbotauroSpaziale <50824099+GibbotauroSpaziale@users.noreply.github.com>
2023-08-08 18:54:00 +0200

    Update GibboBot.py

--- GibboBot.py
+++ GibboBot.py
@@ -21,6 +21,7 @@ def hela():
     client_hela.start()
     
     data = client_hela.chat.send_message('CHAR', message)
+    print(data);
     ms = data['replies'][0]['text']
     
     response = requests.post(url, json={"rply": ms})
