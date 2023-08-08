9f1684fe3bba8032c043485f5cec1baee9e68b2f
GibbotauroSpaziale <50824099+GibbotauroSpaziale@users.noreply.github.com>
2023-08-08 19:32:36 +0200

    Update GibboBot.py

--- GibboBot.py
+++ GibboBot.py
@@ -4,10 +4,10 @@ from urllib.parse import urlparse
 from time import sleep 
 from flask import Flask, request
 import asyncio
-from characterai import PyAsyncCAI
+from characterai import PyAsyncCAI, PyCAI
 
 app = Flask(__name__)
-
+"""
 async def asyncHela(message):
     client_hela = PyAsyncCAI('aciHa-7oXJih543TrBrR-O_PiZN-8lrdb9s4Bb6_73Q')
     print("client_hela init")
@@ -17,7 +17,7 @@ async def asyncHela(message):
     m = data['replies'][0]['text']
     print("message", m)
     return m
-
+"""
 @app.route("/")
 def index():
     return "OK"
@@ -25,8 +25,13 @@ def index():
 @app.route("/Hela")
 def hela():
     message = request.args.get('message')
+
+    client = PyCAI('aciHa-7oXJih543TrBrR-O_PiZN-8lrdb9s4Bb6_73Q')
+    data = client.chat.send_message('CHAR', message)
     
-    return asyncHela(message)
+    ms = data['replies'][0]['text']
+
+    return ms #asyncHela(message)
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
