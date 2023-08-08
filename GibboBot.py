212b890ae85697a85fb503437757fb62dabedc6e
GibbotauroSpaziale <50824099+GibbotauroSpaziale@users.noreply.github.com>
2023-08-08 19:27:38 +0200

    Update GibboBot.py

--- GibboBot.py
+++ GibboBot.py
@@ -10,8 +10,9 @@ app = Flask(__name__)
 
 async def asyncHela(message):
     client_hela = PyAsyncCAI('aciHa-7oXJih543TrBrR-O_PiZN-8lrdb9s4Bb6_73Q')
+    print("client_hela init")
     await client_hela.start()
-    print("message", message)
+    print("client_hela started")
     data = await client.chat.send_message('CHAR', message)
     m = data['replies'][0]['text']
     print("message", m)
@@ -28,5 +29,4 @@ def hela():
     return asyncHela(message)
     
 if __name__ == "__main__":
-    asyncio.run(asyncHela())
     app.run(host='0.0.0.0', port=3000, debug=True)
